73e2fa6497206a5126e8c524e04858fdf7def27d
risan <risanbagja@gmail.com>
2018-11-13 21:47:56 +0100

    refactor: use one return statement

--- src/get-event-type.js
+++ src/get-event-type.js
@@ -8,76 +8,45 @@ const has = require("lodash/has");
  * @return {String}
  */
 const getEventType = event => {
+  let type = "unknown";
+
   if (has(event, "message")) {
-    return get(event, "message.is_echo") ? "echo" : "message";
+    type = get(event, "message.is_echo") ? "echo" : "message";
+  } else if (has(event, "account_linking")) {
+    type = "account-linking";
+  } else if (has(event, "checkout_update")) {
+    type = "checkout-update";
+  } else if (has(event, "delivery")) {
+    type = "delivered";
+  } else if (has(event, "game_play")) {
+    type = "game-play";
+  } else if (has(event, "optin")) {
+    type = "optin";
+  } else if (has(event, "payment")) {
+    type = "payment";
+  } else if (has(event, "policy-enforcement")) {
+    type = "policy-enforcement";
+  } else if (has(event, "postback")) {
+    type = "postback";
+  } else if (has(event, "pre_checkout")) {
+    type = "pre-checkout";
+  } else if (has(event, "read")) {
+    type = "read";
+  } else if (has(event, "referral")) {
+    type = "referral";
+  } else if (has(event, "standby")) {
+    type = "standby";
+  } else if (has(event, "app_roles")) {
+    type = "handover.app-roles";
+  } else if (has(event, "pass_thread_control")) {
+    type = "handover.pass-thread-control";
+  } else if (has(event, "take_thread_control")) {
+    type = "handover.take-thread-control";
+  } else if (has(event, "request_thread_control")) {
+    type = "handover.request-thread-control";
   }
 
-  if (has(event, "account_linking")) {
-    return "account-linking";
-  }
-
-  if (has(event, "checkout_update")) {
-    return "checkout-update";
-  }
-
-  if (has(event, "delivery")) {
-    return "delivered";
-  }
-
-  if (has(event, "game_play")) {
-    return "game-play";
-  }
-
-  if (has(event, "optin")) {
-    return "optin";
-  }
-
-  if (has(event, "payment")) {
-    return "payment";
-  }
-
-  if (has(event, "policy-enforcement")) {
-    return "policy-enforcement";
-  }
-
-  if (has(event, "postback")) {
-    return "postback";
-  }
-
-  if (has(event, "pre_checkout")) {
-    return "pre-checkout";
-  }
-
-  if (has(event, "read")) {
-    return "read";
-  }
-
-  if (has(event, "referral")) {
-    return "referral";
-  }
-
-  if (has(event, "standby")) {
-    return "standby";
-  }
-
-  // Handover events.
-  if (has(event, "app_roles")) {
-    return "handover.app-roles";
-  }
-
-  if (has(event, "pass_thread_control")) {
-    return "handover.pass-thread-control";
-  }
-
-  if (has(event, "take_thread_control")) {
-    return "handover.take-thread-control";
-  }
-
-  if (has(event, "request_thread_control")) {
-    return "handover.request-thread-control";
-  }
-
-  return "unknown";
+  return type;
 };
 
 module.exports = getEventType;
