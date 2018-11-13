c408316087ce74cbb6ef76bbc9893634b8e7f8e6
risan <risanbagja@gmail.com>
2018-11-13 18:54:02 +0100

    feat: Add the main middleware

---
+++ __tests__/index.js
@@ -0,0 +1,28 @@
+/* global jest: false, test:false, expect:false */
+const express = require("express");
+
+const fbmWebhook = require("../src");
+const verification = require("../src/verification");
+const webhook = require("../src/webhook");
+
+jest.mock("express");
+jest.mock("../src/verification");
+jest.mock("../src/webhook");
+
+test("it add verification and webhook middlewares", () => {
+  const app = { use: jest.fn() };
+
+  express.mockReturnValue(app);
+  verification.mockReturnValue("verification");
+  webhook.mockReturnValue("webhook");
+
+  fbmWebhook({ path: "/webhook", verifyToken: "secret" });
+
+  expect(express).toHaveBeenCalledTimes(1);
+  expect(verification).toHaveBeenCalledWith("secret");
+  expect(webhook).toHaveBeenCalledWith(app);
+
+  expect(app.use).toHaveBeenCalledTimes(2);
+  expect(app.use).toHaveBeenNthCalledWith(1, "/webhook", "verification");
+  expect(app.use).toHaveBeenNthCalledWith(2, "/webhook", "webhook");
+});
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const verification = require("./verification");
+const webhook = require("./webhook");
+
+const fbmWebhook = ({
+  path = "/",
+  verifyToken = process.env.FB_VERIFY_TOKEN
+}) => {
+  const app = express();
+
+  app.use(path, verification(verifyToken));
+  app.use(path, webhook(app));
+
+  return app;
+};
+
+module.exports = fbmWebhook;
